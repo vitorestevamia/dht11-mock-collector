4d6086944744cf4c4be182d31ea74454dd1a38f3
Vitor Estevam <vitor_estevam@atlantico.com.br>
2023-05-04 17:05:21 -0300

    addin random measurements

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, `{"temperature": 10, "humidity": 68}`)
+		fmt.Fprintf(w, `{"temperature": %v, "humidity": %v}`, rand.Intn(40-20)+20, rand.Intn(60-10)+10)
 	})
 
 	http.ListenAndServe(":5000", nil)
